7460c2b5640dd336efa71596d852243286749c85
Kevin Gibbons <bakkot@gmail.com>
2017-02-14 01:19:26 -0800

    implement, with examples

---
+++ lindenmayer.js
@@ -0,0 +1,94 @@
+"use strict";
+
+class LSystem {
+	iter(str) {
+		return Array.prototype.map.call(str, c => this.production[c]).join('');
+	}
+
+	constructor(axiom, production) {
+		this.axiom = axiom;
+		this.production = Object.assign(Object.create(null), production);
+
+		for (const rhs of Object.values(production)) {
+			for (const c of rhs) {
+				if (typeof this.production[c] !== 'undefined') continue;
+				this.production[c] = [c];
+			}
+		}
+	}
+}
+
+
+function turtleBound(string, delta, alpha) {
+	let x = 0,
+			y = 0,
+			xMin = 0,
+			xMax = 0,
+			yMin = 0,
+			yMax = 0;
+
+	for (const c of string) {
+		if (c === '+') {
+		  alpha += delta;
+		} else if (c === '-') {
+		  alpha -= delta;
+		} else {
+			let code = c.charCodeAt(0);
+			if (code >= 65 && code <= 90 || code >= 97 && code <= 122) { // A-Z, a-z
+				x += Math.cos(alpha);
+				y += Math.sin(alpha);
+				xMin = Math.min(xMin, x);
+				xMax = Math.max(xMax, x);
+				yMin = Math.min(yMin, y);
+				yMax = Math.max(yMax, y);
+			}
+		}
+	}
+	return {xMin, xMax, yMin, yMax};
+}
+
+function turtleDraw(ctx, string, delta, alpha, scale) {
+	let {xMin, xMax, yMin, yMax} = turtleBound(string, delta, alpha);
+	let bound = Math.max(xMax - xMin, yMax - yMin);
+	let d = scale / bound;
+	let x = (-xMin + (bound - (xMax - xMin)) / 2) * d;
+	let y = (-yMin + (bound - (yMax - yMin)) / 2) * d;
+
+	ctx.beginPath();
+	ctx.moveTo(x, scale - y);
+	for (const c of string) {
+		if (c === '+') {
+		  alpha += delta;
+		} else if (c === '-') {
+		  alpha -= delta;
+		} else {
+			let code = c.charCodeAt(0);
+			if (code >= 65 && code <= 90) { // A-Z
+				x += d * Math.cos(alpha);
+				y += d * Math.sin(alpha);
+				ctx.lineTo(x, scale - y);
+			} else if (code >= 97 && code <= 122) {
+				x += d * Math.cos(alpha);
+				y += d * Math.sin(alpha);
+				ctx.moveTo(x, scale - y);
+			}
+		}
+	}
+	ctx.stroke();
+}
+
+function turtleStepper({system, delta, alpha = 0, ctx, clear, scale}) {
+	let string = system.axiom;
+
+	let first = true;
+	return function() {
+		clear();
+		if (first) {
+			first = false;
+			turtleDraw(ctx, string, delta, alpha, scale);
+		} else {
+			string = system.iter(string);
+			turtleDraw(ctx, string, delta, alpha, scale);
+		}
+	}
+}
---
+++ main.js
@@ -0,0 +1,195 @@
+"use strict";
+
+
+let scale, clear;
+
+
+const quadKochStepper = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F-F-F-F',
+		{
+			F: 'F-F+F+FF-F-F+F'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const quadFlake = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'-F',
+		{
+			F: 'F+F-F-F+F'
+		}
+	),
+	delta: Math.PI / 2,
+	alpha: -Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const islandLake = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F+F+F+F',
+		{
+			F: 'F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF',
+			f: 'ffffff'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const koch1 = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F-F-F-F',
+		{
+			F: 'FF-F-F-F-F-F+F'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const koch2 = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F-F-F-F',
+		{
+			F: 'FF-F-F-F-FF'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const koch3 = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F-F-F-F',
+		{
+			F: 'FF-F+F-F-FF'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const koch4 = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F-F-F-F',
+		{
+			F: 'FF-F--F-F'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const koch5 = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F-F-F-F',
+		{
+			F: 'F-FF--F-F'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const koch6 = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F-F-F-F',
+		{
+			F: 'F-F+F-F-F'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const dragon = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F',
+		{
+			F: 'F+G+',
+			G: '-F-G'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+const sierp = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F',
+		{
+			F: 'G+F+G',
+			G: 'F-G-F'
+		}
+	),
+	delta: Math.PI / 3,
+	ctx,
+	clear,
+	scale
+});
+
+const hexGopher = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'F',
+		{
+			F: 'F+G++G-F--FF-G+',
+			G: '-F+GG++G+F--F-G'
+		}
+	),
+	delta: Math.PI / 3,
+	ctx,
+	clear,
+	scale
+});
+
+const quadGopher = (ctx, clear, scale) => turtleStepper({
+	system: new LSystem(
+		'-G',
+		{
+			F: 'FF-G-G+F+F-G-GF+G+FFG-F+G+FF+G-FG-G-F+F+GG-',
+			G: '+FF-G-G+F+FG+F-GG-F-G+FGG-F-GF+F+G-G-F+F+GG'
+		}
+	),
+	delta: Math.PI / 2,
+	ctx,
+	clear,
+	scale
+});
+
+addEventListener('load', () => {
+	const canvas = document.querySelector('canvas');
+	const ctx = canvas.getContext('2d');
+	const scale = canvas.width - 4;
+	ctx.translate(2, 2);
+
+	function clear() {
+		ctx.clearRect(-2, -2, scale + 4, scale + 4);
+	};
+
+	const step = quadGopher(ctx, clear, scale);
+
+	canvas.addEventListener('click', step);
+});
