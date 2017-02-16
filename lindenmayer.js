f28e21c843e316bdea8ccddfc7c59db1b7910aa6
Kevin Gibbons <bakkot@gmail.com>
2017-02-16 00:07:06 -0800

    refactor

--- lindenmayer.js
+++ lindenmayer.js
@@ -19,87 +19,98 @@ class LSystem {
 }
 
 
-function turtleBound(string, delta, alpha) {
-	let x = 0,
-			y = 0,
-			xMin = 0,
-			xMax = 0,
-			yMin = 0,
-			yMax = 0;
+class TurtleSystem extends LSystem {
+	constructor(axiom, production, delta, alpha = 0) {
+		super(axiom, production);
+		this.delta = delta;
+		this.alpha = alpha;
+	}
 
-	let stack = [];
-	for (const c of string) {
-		if (c === '+') {
-		  alpha += delta;
-		} else if (c === '-') {
-		  alpha -= delta;
-		} else if (c === '[') {
-			stack.push({x, y, alpha});
-		} else if (c === ']') {
-			({x, y, alpha} = stack.pop());
-		} else {
-			let code = c.charCodeAt(0);
-			if (code >= 65 && code <= 90 || code >= 97 && code <= 122) { // A-Z, a-z
-				x += Math.cos(alpha);
-				y += Math.sin(alpha);
-				xMin = Math.min(xMin, x);
-				xMax = Math.max(xMax, x);
-				yMin = Math.min(yMin, y);
-				yMax = Math.max(yMax, y);
+	getStepper(ctx, clear, scale) {
+		let {delta, alpha, iter} = this;
+		iter = iter.bind(this);
+		let string = this.axiom;
+
+		let first = true;
+		return function() {
+			clear();
+			if (first) {
+				first = false;
+				TurtleSystem.draw(ctx, scale, string, delta, alpha);
+			} else {
+				string = iter(string);
+				TurtleSystem.draw(ctx, scale, string, delta, alpha);
 			}
 		}
 	}
-	return {xMin, xMax, yMin, yMax};
-}
 
-function turtleDraw(ctx, string, delta, alpha, scale) {
-	let {xMin, xMax, yMin, yMax} = turtleBound(string, delta, alpha);
-	let bound = Math.max(xMax - xMin, yMax - yMin);
-	let d = scale / bound;
-	let x = (-xMin + (bound - (xMax - xMin)) / 2) * d;
-	let y = (-yMin + (bound - (yMax - yMin)) / 2) * d;
+	static bound(string, delta, alpha) {
+		// TODO merge with draw?
+		let x = 0,
+				y = 0,
+				xMin = 0,
+				xMax = 0,
+				yMin = 0,
+				yMax = 0;
 
-	let stack = [];
-	ctx.beginPath();
-	ctx.moveTo(x, scale - y);
-	for (const c of string) {
-		if (c === '+') {
-		  alpha += delta;
-		} else if (c === '-') {
-		  alpha -= delta;
-		} else if (c === '[') {
-			stack.push({x, y, alpha});
-		} else if (c === ']') {
-			({x, y, alpha} = stack.pop());
-			ctx.moveTo(x, scale - y);
-		} else {
-			let code = c.charCodeAt(0);
-			if (code >= 65 && code <= 90) { // A-Z
-				x += d * Math.cos(alpha);
-				y += d * Math.sin(alpha);
-				ctx.lineTo(x, scale - y);
-			} else if (code >= 97 && code <= 122) {
-				x += d * Math.cos(alpha);
-				y += d * Math.sin(alpha);
+		let stack = [];
+		for (const c of string) {
+			if (c === '+') {
+			  alpha += delta;
+			} else if (c === '-') {
+			  alpha -= delta;
+			} else if (c === '[') {
+				stack.push({x, y, alpha});
+			} else if (c === ']') {
+				({x, y, alpha} = stack.pop());
+			} else {
+				let code = c.charCodeAt(0);
+				if (code >= 65 && code <= 90 || code >= 97 && code <= 122) { // A-Z, a-z
+					x += Math.cos(alpha);
+					y += Math.sin(alpha);
+					xMin = Math.min(xMin, x);
+					xMax = Math.max(xMax, x);
+					yMin = Math.min(yMin, y);
+					yMax = Math.max(yMax, y);
+				}
+			}
+		}
+		return {xMin, xMax, yMin, yMax};
+	}
+
+	static draw(ctx, scale, string, delta, alpha) {
+		let {xMin, xMax, yMin, yMax} = TurtleSystem.bound(string, delta, alpha);
+		let bound = Math.max(xMax - xMin, yMax - yMin);
+		let d = scale / bound;
+		let x = (-xMin + (bound - (xMax - xMin)) / 2) * d;
+		let y = (-yMin + (bound - (yMax - yMin)) / 2) * d;
+
+		let stack = [];
+		ctx.beginPath();
+		ctx.moveTo(x, scale - y);
+		for (const c of string) {
+			if (c === '+') {
+			  alpha += delta;
+			} else if (c === '-') {
+			  alpha -= delta;
+			} else if (c === '[') {
+				stack.push({x, y, alpha});
+			} else if (c === ']') {
+				({x, y, alpha} = stack.pop());
 				ctx.moveTo(x, scale - y);
+			} else {
+				let code = c.charCodeAt(0);
+				if (code >= 65 && code <= 90) { // A-Z
+					x += d * Math.cos(alpha);
+					y += d * Math.sin(alpha);
+					ctx.lineTo(x, scale - y);
+				} else if (code >= 97 && code <= 122) { // a-z
+					x += d * Math.cos(alpha);
+					y += d * Math.sin(alpha);
+					ctx.moveTo(x, scale - y);
+				}
 			}
 		}
-	}
-	ctx.stroke();
-}
-
-function turtleStepper({system, delta, alpha = 0, ctx, clear, scale}) {
-	let string = system.axiom;
-
-	let first = true;
-	return function() {
-		clear();
-		if (first) {
-			first = false;
-			turtleDraw(ctx, string, delta, alpha, scale);
-		} else {
-			string = system.iter(string);
-			turtleDraw(ctx, string, delta, alpha, scale);
-		}
+		ctx.stroke();
 	}
 }
--- main.js
+++ main.js
@@ -28,184 +28,119 @@ const quadFlake = (ctx, clear, scale) => turtleStepper({
 	scale
 });
 
-const islandLake = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F+F+F+F',
-		{
-			F: 'F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF',
-			f: 'ffffff'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const islandLake = new TurtleSystem(
+	'F+F+F+F',
+	{
+		F: 'F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF',
+		f: 'ffffff'
+	},
+	Math.PI / 2
+);
 
-const koch1 = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F-F-F-F',
-		{
-			F: 'FF-F-F-F-F-F+F'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const koch1 = new TurtleSystem(
+	'F-F-F-F',
+	{
+		F: 'FF-F-F-F-F-F+F'
+	},
+	Math.PI / 2
+);
 
-const koch2 = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F-F-F-F',
-		{
-			F: 'FF-F-F-F-FF'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const koch2 = new TurtleSystem(
+	'F-F-F-F',
+	{
+		F: 'FF-F-F-F-FF'
+	},
+	Math.PI / 2
+);
 
-const koch3 = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F-F-F-F',
-		{
-			F: 'FF-F+F-F-FF'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const koch3 = new TurtleSystem(
+	'F-F-F-F',
+	{
+		F: 'FF-F+F-F-FF'
+	},
+	Math.PI / 2
+);
 
-const koch4 = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F-F-F-F',
-		{
-			F: 'FF-F--F-F'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const koch4 = new TurtleSystem(
+	'F-F-F-F',
+	{
+		F: 'FF-F--F-F'
+	},
+	Math.PI / 2
+);
 
-const koch5 = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F-F-F-F',
-		{
-			F: 'F-FF--F-F'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const koch5 = new TurtleSystem(
+	'F-F-F-F',
+	{
+		F: 'F-FF--F-F'
+	},
+	Math.PI / 2
+);
 
-const koch6 = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F-F-F-F',
-		{
-			F: 'F-F+F-F-F'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const koch6 = new TurtleSystem(
+	'F-F-F-F',
+	{
+		F: 'F-F+F-F-F'
+	},
+	Math.PI / 2
+);
 
-const dragon = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F',
-		{
-			F: 'F+G+',
-			G: '-F-G'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const dragon = new TurtleSystem(
+	'F',
+	{
+		F: 'F+G+',
+		G: '-F-G'
+	},
+	Math.PI / 2
+);
 
-const sierp = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F',
-		{
-			F: 'G+F+G',
-			G: 'F-G-F'
-		}
-	),
-	delta: Math.PI / 3,
-	ctx,
-	clear,
-	scale
-});
+const sierp = new TurtleSystem(
+	'F',
+	{
+		F: 'G+F+G',
+		G: 'F-G-F'
+	},
+	Math.PI / 3
+);
 
-const hexGopher = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F',
-		{
-			F: 'F+G++G-F--FF-G+',
-			G: '-F+GG++G+F--F-G'
-		}
-	),
-	delta: Math.PI / 3,
-	ctx,
-	clear,
-	scale
-});
+const hexGopher = new TurtleSystem(
+	'F',
+	{
+		F: 'F+G++G-F--FF-G+',
+		G: '-F+GG++G+F--F-G'
+	},
+	Math.PI / 3
+);
 
-const quadGopher = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'-G',
-		{
-			F: 'FF-G-G+F+F-G-GF+G+FFG-F+G+FF+G-FG-G-F+F+GG-',
-			G: '+FF-G-G+F+FG+F-GG-F-G+FGG-F-GF+F+G-G-F+F+GG'
-		}
-	),
-	delta: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const quadGopher = new TurtleSystem(
+	'-G',
+	{
+		F: 'FF-G-G+F+F-G-GF+G+FFG-F+G+FF+G-FG-G-F+F+GG-',
+		G: '+FF-G-G+F+FG+F-GG-F-G+FGG-F-GF+F+G-G-F+F+GG'
+	},
+	Math.PI / 2
+);
 
-const bush1 = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'F',
-		{
-			F: 'F[+F]F[-F]F'
-		}
-	),
-	delta: Math.PI / 7,
-	alpha: Math.PI / 2,
-	ctx,
-	clear,
-	scale
-});
+const bush1 = new TurtleSystem(
+	'F',
+	{
+		F: 'F[+F]F[-F]F'
+	},
+	Math.PI / 7,
+	Math.PI / 2
+);
 
-const penrose = (ctx, clear, scale) => turtleStepper({
-	system: new LSystem(
-		'[7]++[7]++[7]++[7]++[7]',
-		{
-			F: '',
-			6: '8F++9F----7F[-8F----6F]++',
-			7: '+8F--9F[---6F--7F]+',
-			8: '-6F++7F[+++8F++9F]-',
-			9: '--8F++++6F[+9F++++7F]--7F'
-		}
-	),
-	delta: Math.PI / 5,
-	ctx,
-	clear,
-	scale
-});
+const penrose = new TurtleSystem(
+	'[7]++[7]++[7]++[7]++[7]',
+	{
+		F: '',
+		6: '8F++9F----7F[-8F----6F]++',
+		7: '+8F--9F[---6F--7F]+',
+		8: '-6F++7F[+++8F++9F]-',
+		9: '--8F++++6F[+9F++++7F]--7F'
+	},
+	Math.PI / 5
+);
 
 addEventListener('load', () => {
 	const canvas = document.querySelector('canvas');
@@ -217,7 +152,7 @@ addEventListener('load', () => {
 		ctx.clearRect(-2, -2, scale + 4, scale + 4);
 	};
 
-	const step = penrose(ctx, clear, scale);
+	const step = islandLake.getStepper(ctx, clear, scale);
 
 	canvas.addEventListener('click', step);
 });
